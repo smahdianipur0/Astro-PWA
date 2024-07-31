de1305f11850698e56e10e1a40bd5127983da0b0
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2024-07-31 22:18:02 +0430

    Google Workbox

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 import { defineConfig } from 'astro/config';
 
-import alpinejs from "@astrojs/alpinejs";
 
-import AstroPWA from '@vite-pwa/astro'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(),
-
-    AstroPWA({
-      devOptions: {
-        enabled: true }
-
-    })
-
-
-    ]
+  integrations: [
+    solidJs()
+    ],
+  vite: {
+    plugins: [wasm(), topLevelAwait()],
+    css: { transformer: 'lightningcss' },
+  },
+  build: {
+    assetsInlineLimit: 51200,
+    inlineStylesheets: 'always',
+  },
 });
